823a3818e9432174214bd6ca9add6fb7f974755f
donSchoe <afri.alex@gmail.com>
2015-03-05 15:45:34 +0100

    Use shortcuts.

--- aptroomat/views.py
+++ aptroomat/views.py
@@ -1,20 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import RequestContext, loader
-
 
 def index(request):
     enter = "start exploring"
-    template = loader.get_template('aptroomat/index.html')
-    context = RequestContext(request, {
-        'enter': enter,
-    })
-    return HttpResponse(template.render(context))
+    context = { 'enter': enter }
+    return render(request, 'aptroomat/index.html', context)
 
 def explore(request):
     enter = "can\'t stop exploring"
-    template = loader.get_template('aptroomat/index.html')
-    context = RequestContext(request, {
-        'enter': enter,
-    })
-    return HttpResponse(template.render(context))
+    context = { 'enter': enter }
+    return render(request, 'aptroomat/index.html', context)
