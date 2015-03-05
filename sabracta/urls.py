6b3a434ff2b1bd7f48d2ebfac889e21f8f396467
donSchoe <afri.alex@gmail.com>
2015-03-05 12:38:32 +0100

    Create index view.

--- sabracta/urls.py
+++ sabracta/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, include, url
 
 # use geodjango admin instead of default
 from django.contrib.gis import admin
+import aptroomat
 
 urlpatterns = patterns('',
+    url(r'^aptroomat/', include('aptroomat.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
